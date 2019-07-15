b08a27d647acd29c9da6d00d29c0099bec44abd0
afaf-tech <fatihalfikri5@google.com>
2019-07-16 04:03:44 +0700

    Mengelola folder dan mengumpulkan tugas2.js

---
+++ tugas/tugas2.js
@@ -0,0 +1,11 @@
+var str = "Ini string";
+var bool = true;
+var kosong = null;
+var kos2 = undefined;
+var arr = [1,2,3,4,5,6,3];
+
+console.log(str);
+console.log(bool);
+console.log(kosong);
+console.log(kos2);
+console.log(arr);
